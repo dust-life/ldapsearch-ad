e67a540dcbfa4669a698b990739138379a93a92f
yaap7 <gracieux.guillaume@gmail.com>
2024-12-10 17:44:48 +0100

    add -b option, line length to 130 chars, a few minors improvements

--- ldapsearchad/exceptions.py
+++ ldapsearchad/exceptions.py
@@ -1,5 +1,5 @@
 class LsaException(Exception):
     """This is a custom Exception to raise when errors occurs in LdapsearchAd class
-    lsa is the acronym of LdapSearchAd"""
+    LSA is the acronym of LdapSearchAd"""
 
     pass
